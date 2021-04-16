4447d2e3889b6ea76912ea70d898afbab901f951
marcantoine <marcantoine.guery@gmail.com>
2021-04-16 20:06:13 +0200

    Reorganised code

--- Algorithms/vargene.AssemblyPipeline.masurca.PostImprovement.NoReadextraction.sh
+++ Algorithms/vargene.AssemblyPipeline.masurca.PostImprovement.NoReadextraction.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+####Modified by marcguery on Fri 16 Apr 17:33:43 CEST 2021########
+
+
+################################PARAMETERS################################
+
 name=$1
-
-
-### Version 4: read correction
-#set -e
+bam=$name.bam
 
 if [ ! -d "Assembly.$name" ]; then
 	echo "Direcoty does not exist";
@@ -11,92 +13,148 @@ if [ ! -d "Assembly.$name" ]; then
 fi
 
 cd Assembly.$name
+
 if [ ! -f "SC.$name.fasta" ]; then
 	echo "SC.$name.fasta does not exist";
 	exit 1;
 fi
 
-threads=4;
+threads=4
+
 SMALT_PARAMETER=" -n $threads "; export SMALT_PARAMETER
 ICORN2_THREADS=$threads; export ICORN2_THREADS;
-
+REAPR_AGRESSIVE_BREAKING=" -a "; export REAPR_AGRESSIVE_BREAKING
 
 finalDIR=$(pwd)
 deleteIT=0
 
-tmp=$$;
+kmer=71
+
+################################################################
+
+
+################################CONFIG################################
+
+##Data files
+tmp=$$
 olddir=$PWD
 dir="/tmp/tdo/dir.tdo.$tmp/"
 #dir="local"
-echo "Old dir $olddir   New DirTEST $dir" 
+RAWREADS1=rawreads_1.fastq
+RAWREADS2=rawreads_2.fastq
+
+
+
+##Tools
+
+#Commons
+FORMATDB=formatdb
+MEGABLAST=megablast
+
+#sh
+CORRECTLINE=bam.correctLineLength.sh
+ASSSTATS=~tdo/Bin/assstats.sh
+ICORN2SERIAL=/nfs/users/nfs_t/tdo/Bin/icorn2.serial.sh
+ANNOT1=~/Bin/vargene.AnnotationVARgenes.assembly.V0.1.sh
+ANNOT2=~tdo/Bin/vargene.AnnotationVARgenes.assembly.V0.2.cov.sh
+
+#Python
+BSUB=bsub.py
+
+#Perl
+HELPERPUTLENGTH=/nfs/users/nfs_t/tdo/Bin/helper.putlengthfasta2Blastm8.pl
+OVERLAPCHECKER=~/Bin/lav.decontamination_overlapChecker.pl
+CONTIGDELETER=/nfs/users/nfs_t/tdo/Bin/Assembly.deleteContigs.pl
+DELETEBLASTENTRY=~tdo/Bin/pacbio.deleteEntryinBlast.pl
+JOINCONTIGS=/nfs/users/nfs_t/tdo/Bin/gapclosing.joinContigs.pl
+COLLECTRESULTS=~tdo/Bin/icorn2.collectResults.pl
+
+#Others
+PAGIT=/nfs/pathogen003/tdo/Tools/PAGIT/64bit/PAGIT/sourceme.pagit
+LITTLESMALT=~tdo/Bin/little.smalt.bam.sh
+OVERLAPFINDER=~tdo/Bin/findoverlaps_ver3.pl
+IMAGE=$PAGIT_HOME/IMAGE/image.pl
+SSPACE=~tdo/bin/SSPACE-BASIC-2.0_linux-x86_64/SSPACE_Basic_v2.0.pl
+REAPER=~tdo/Bin/assemblypipeline.doreapr.sh
+
+
+################################################################
+
+
+################################SCRIPT################################
+
+### Version 4: read correction
+#set -e
+
+echo "Old dir $olddir  New DirTEST $dir"
 
 mkdir -p $dir
 cp SC.$name.fasta $dir
 cd $dir
 
-bam=$name.bam
-
 ### link the bam file over
 echo "cp -s $olddir/$bam* ../"
-
 
 # General test
 insertSize=350
 
-ln -s $olddir/rawreads_1.fastq rawreads_1.fastq
-ln -s $olddir/rawreads_2.fastq rawreads_2.fastq
+ln -s $olddir/$RAWREADS1 $RAWREADS1
+ln -s $olddir/$RAWREADS2 $RAWREADS2
 
 ### 
 fastq=$bam
 
 ### scaffaolding
 ### gapwalking Take the 
-source /nfs/pathogen003/tdo/Tools/PAGIT/64bit/PAGIT/sourceme.pagit
+source $PAGIT
 
 
 cp SC.$name.fasta  01.assembly.fa
 ### Do the merging stuff of contigs, and delete potential contained...
-formatdb -p F -i 01.assembly.fa
-megablast -W 40 -F F -a $threads -m 8 -e 1e-80 -d 01.assembly.fa -i 01.assembly.fa | awk '$3>99 && $4>200 && $1 != $2' > comp.self1.blast
-perl /nfs/users/nfs_t/tdo/Bin/helper.putlengthfasta2Blastm8.pl 01.assembly.fa 01.assembly.fa comp.self1.blast &> /dev/null
+$FORMATDB -p F -i 01.assembly.fa
+$MEGABLAST -W 40 -F F -a $threads -m 8 -e 1e-80 -d 01.assembly.fa -i 01.assembly.fa | \
+	awk '$3>99 && $4>200 && $1 != $2' > comp.self1.blast
+perl $HELPERPUTLENGTH 01.assembly.fa 01.assembly.fa comp.self1.blast &> /dev/null
 
 
 
 # 2a: delete contained contigs
 # we want the query to be always the smaller one
-awk '$3>99 && $4>500 && $13 < $14' comp.self1.blast.length | perl ~/Bin/lav.decontamination_overlapChecker.pl > 02.ListContained.txt
+awk '$3>99 && $4>500 && $13 < $14' comp.self1.blast.length | \
+	perl $OVERLAPCHECKER > 02.ListContained.txt
 cat 02.ListContained.txt | awk '$4>90' | cut -f 1 > List.Contained.fofn
 
-perl /nfs/users/nfs_t/tdo/Bin/Assembly.deleteContigs.pl List.Contained.fofn 01.assembly.fa 02.assembly.fa
+perl $CONTIGDELETER List.Contained.fofn 01.assembly.fa 02.assembly.fa
 ### now delete the contigs from the blast
 # also filter for just hits > 2kb > 99%
-cat comp.self1.blast.length | awk '$3> 99.5 && $4 > 500' | perl ~tdo/Bin/pacbio.deleteEntryinBlast.pl List.Contained.fofn > Blast.merge.blast.length
+cat comp.self1.blast.length | \
+	awk '$3> 99.5 && $4 > 500' | \
+	perl $DELETEBLASTENTRY List.Contained.fofn > Blast.merge.blast.length
 
 # 2b: find overlaps 
 ### Get a bam files to do the merge
-~tdo/Bin/little.smalt.bam.sh 02.assembly.fa 20 3 rawreads\_1.fastq rawreads\_2.fastq first 800 
+$LITTLESMALT 02.assembly.fa 20 3 "$RAWREADS1" "$RAWREADS2" first 800 
 
-perl ~tdo/Bin/findoverlaps_ver3.pl Blast.merge.blast.length first.bam 02.assembly.fa OUT
+perl $OVERLAPFINDER Blast.merge.blast.length first.bam 02.assembly.fa OUT
 mv mergedseq_OUT.fasta 03.assembly.fa
 rm first.bam*
 ###IMAGE STEPS
 
 ln -s  03.assembly.fa Res.image.1.fasta 
 ### iterative image
-kmer=71
 mkdir IMAGE;
 cd IMAGE;
-
-
 cp ../Res.image.1.fasta .
-$PAGIT_HOME/IMAGE/image.pl -scaffolds Res.image.1.fasta  -prefix ../rawreads -iteration 1 -all_iteration 2 -dir_prefix ite -kmer $kmer -smalt_minScore 65 &> Image.output.txt
-perl /nfs/users/nfs_t/tdo/Bin/gapclosing.joinContigs.pl ite2/new.fa ite2/new.read.placed Res.image.3
+
+$IMAGE -scaffolds Res.image.1.fasta -prefix ../rawreads -iteration 1 -all_iteration 2 -dir_prefix ite \
+-kmer $kmer -smalt_minScore 65 &> Image.output.txt
+perl $JOINCONTIGS ite2/new.fa ite2/new.read.placed Res.image.3
 rm  Image.output.txt
 
-echo "lib2 ../rawreads_1.fastq ../rawreads_2.fastq $insertSize 0.5 FR" > lib2
-perl ~tdo/bin/SSPACE-BASIC-2.0_linux-x86_64/SSPACE_Basic_v2.0.pl -l lib2  -n 31 -s Res.image.3.fasta  -x 0 -k 10 -b out.sspace > Sspace.output.txt
+echo "lib2 ../$RAWREADS1 ../$RAWREADS2 $insertSize 0.5 FR" > lib2
+perl $SSPACE -l lib2  -n 31 -s Res.image.3.fasta  -x 0 -k 10 -b out.sspace > Sspace.output.txt
 sed 's/|/_/g' out.sspace.final.scaffolds.fasta > ../Res.Step3.fasta
-bam.correctLineLength.sh  ../Res.Step3.fasta
+$CORRECTLINE ../Res.Step3.fasta
 cd ..
 if [ ! -f "Res.Step3.fasta" ] ; then
 	echo "Error in Image"
@@ -109,15 +167,16 @@ fi
 
 ### clean
 rm -rf IMAGE/
-~tdo/Bin/assstats.sh Res.Step3.fasta > stats.IMAGED.txt
+$ASSSTATS Res.Step3.fasta > stats.IMAGED.txt
 
 cp Res.Step3.fasta 04.assembly.fa
 
 ### icorn 2 iterations
-/nfs/users/nfs_t/tdo/Bin/icorn2.serial.sh rawreads 700 Res.Step3.fasta 1 2 &> icorn2.out.txt
-perl ~tdo/Bin/icorn2.collectResults.pl . > Stats.icorn2.txt
-cp ICORN2.Res.Step3.fasta.3  icorned.fasta
+$ICORN2SERIAL rawreads 700 Res.Step3.fasta 1 2 &> icorn2.out.txt
+perl $COLLECTRESULTS . > Stats.icorn2.txt
+cp ICORN2.Res.Step3.fasta.3 icorned.fasta
 rm -rf ICORN2*
+
 if [ ! -f "icorned.fasta" ] ; then
 	echo "Error in icorn..."
 	exit 127;
@@ -126,8 +185,8 @@ fi
 cp icorned.fasta 05.assembly.fa
 
 ### reapr
-REAPR_AGRESSIVE_BREAKING=" -a "; export REAPR_AGRESSIVE_BREAKING
-~tdo/Bin/assemblypipeline.doreapr.sh rawreads icorned.fasta SC.fixed.$name 0 $insertSize
+
+$REAPER rawreads icorned.fasta SC.fixed.$name 0 $insertSize
 rm -rf SC.fixed.$name/
 cp  SC.fixed.$name.fasta  06.assembly.fa
 ### tmp stuff - moving the files over...
@@ -140,11 +199,11 @@ cd $olddir
 rm -rf  $dir
 ## go one up 
 if [ -s "SC.fixed.$name.fasta" ] ; then 
-	echo "Doing the annotation now... ~/Bin/vargene.AnnotationVARgenes.assembly.V0.1.sh SC.fixed.$name.fasta empty.bam $name "
-	bsub.py 2 Annotate.$name bash ~tdo/Bin/vargene.AnnotationVARgenes.assembly.V0.2.cov.sh  SC.fixed.$name.fasta rawreads $name
+	echo "Doing the annotation now... $ANNOT1 SC.fixed.$name.fasta empty.bam $name "
+	$BSUB 2 Annotate.$name bash $ANNOT2  SC.fixed.$name.fasta rawreads $name
 fi
 ### do de novo assembly using predifined k-mer of 71
 
 # rm *fastq *.sam
 
-
+################################################################
--- Algorithms/vargene.AssemblyPipeline.masurca.sh
+++ Algorithms/vargene.AssemblyPipeline.masurca.sh
@@ -1,18 +1,68 @@
 #!/bin/bash
+####Modified by marcguery on Fri 16 Apr 17:33:43 CEST 2021########
+
+
+################################PARAMETERS################################
+
 bam=$1
 kmer=$2
 
 n=$LSB_JOBINDEX
 if [ -z "$n" ] ; then
   n=5;
-  fi
+fi
 
-bam=$( head -n $n MasterList.txt | tail -n 1 | awk '{ print $1}');
-name=$( head -n $n MasterList.txt | tail -n 1 |  awk '{ print $2}');
+bam=$( head -n $n $MASTERLIST | tail -n 1 | awk '{ print $1}');
+name=$( head -n $n $MASTERLIST | tail -n 1 |  awk '{ print $2}');
+
+################################################################
 
 
-### Version 4: read correction
+################################CONFIG################################
+
+##Data files
+MASTERLIST=MasterList.txt
+LITTLEPF3D7=/nfs/pathogen003/tdo/Pfalciparum/3D7/Reference/Feb2015/little.Pf3D7.Feb2015.txt
+LITTLEPF3D7V4=/nfs/pathdata2/Plasmodium/falciparum/3D7/Reference/Little.Pf3D7V4.txt
+BAMFOLDER=/nfs/team112_data00/pf.bwa/bam/
+VARCONFIG=/lustre/scratch118/infgen/team133/tdo/Pfalciparum/VAR/Assembly.Version3/config.txt
+CAFOLDER=CA/
+GAPCLOSEFOLDER=10-gapclose/
+SCFFASTA=genome.scf.fasta
+
+
+##Tools
+#sh
+ANNOTVARGENES=~/Bin/vargene.AnnotationVARgenes.assembly.V0.1.sh
+MASPOSTIMP=~/Bin/vargene.AssemblyPipeline.masurca.PostImprovement.NoReadextraction.sh
+ASSSTATS=~tdo/Bin/assstats.sh
+#Perl
+MERGEOSAM=~/Bin/vargene.mergeOverlappingSamtoolsRegions.pl
+SAMTOFASTQ=~/Bin/sam2fastq.2files.pl
+JOINSOLEX=~tdo/Bin/join.solexaBack.actual.sga.pl
+
+#Common
+SAMTOOLS1_2=samtools1.2
+SAMTOOLS=samtools
+FASTAQ=fastaq
+SGA=sga
+MASURCA=/nfs/pathogen003/tdo/bin/MaSuRCA-2.3.2/bin/masurca
+VELVETH=~/bin/velveth
+
+#Others
+BSUB=bsub
+VELVETHTDO=~tdo/bin/velveth.tdo
+VELVETG=~/bin/velvetg
+VELVETGTDO=~tdo/bin/velvetg.tdo
+
+################################################################
+
+
+################################SCRIPT################################
+
+####Version 4: read correction####
 #set -e
+
 if [ -z "$bam" ] ; then
 	echo "Please don't use that script!"
 	exit 1;
@@ -23,23 +73,27 @@ if [ -z "$kmer" ] ; then
 fi
 
 if [[ $bam == *cram* ]] ; then
-   samtools1.2 view -F 2304 -b $bam > $name.bam 
-   
+   $SAMTOOLS1_2 view -F 2304 -b $bam > $name.bam 
 else
-ln -s $bam $name.bam
+	ln -s $bam $name.bam
 fi
-samtools index $name.bam
+
+$SAMTOOLS index $name.bam
 #ln -s $bam.bai $name.bam.bai
+
 echo "$n -- $bam\n";
 #name=$(echo $bam | sed 's/.bam//g')
-mkdir Assembly.$name
+
+mkdir -p Assembly.$name
 cd Assembly.$name
+
 finalDIR=$(pwd)
 deleteIT=0
 lfs setstripe -c -1 .
+
 ###
 ###test 3D7/
-#samtools view -F 4 ../$bam  MAL4:171451-180412  > MappedVAR.$bam.sam
+#$SAMTOOLS view -F 4 ../$bam  MAL4:171451-180412  > MappedVAR.$bam.sam
 
 bam=$name.bam
 
@@ -48,113 +102,162 @@ insertSize=350
 
 
 # ADAPT ofr _v3 self mappeind
-region1=$(egrep "stevor|PIR|PfEMP1" /nfs/pathogen003/tdo/Pfalciparum/3D7/Reference/Feb2015/little.Pf3D7.Feb2015.txt | perl -nle '@ar=split(/\t/); @bb=split(/\.\./,$ar[4]);($a) = $bb[0] =~ /\(*(\d+)/; ($b)= $bb[(scalar(@bb)-1)] =~ /(\d+)\)*/; $chr=substr($ar[0],3,5); print "Pf3$chr:".($a-500)."-".($b+500)'  | awk '{ s=s" "$1} END { print s }' | perl ~/Bin/vargene.mergeOverlappingSamtoolsRegions.pl )
-region=$(egrep "stevor|PIR|PfEMP1" /nfs/pathogen003/tdo/Pfalciparum/3D7/Reference/Feb2015/little.Pf3D7.Feb2015.txt | perl -nle '@ar=split(/\t/); @bb=split(/\.\./,$ar[4]);($a) = $bb[0] =~ /\(*(\d+)/; ($b)= $bb[(scalar(@bb)-1)] =~ /(\d+)\)*/; $chr=substr($ar[0],3,5); print "Pf3$chr\_v3:".($a-500)."-".($b+500)'  | awk '{ s=s" "$1} END { print s }' | perl ~/Bin/vargene.mergeOverlappingSamtoolsRegions.pl )
-region2=$(egrep "stevor|PIR|PfEMP1|rif" /nfs/pathdata2/Plasmodium/falciparum/3D7/Reference/Little.Pf3D7V4.txt | perl -nle '@ar=split(/\t/); @bb=split(/\.\./,$ar[4]);($a) = $bb[0] =~ /\(*(\d+)/; ($b)= $bb[(scalar(@bb)-1)] =~ /(\d+)\)*/; $chr=substr($ar[0],3,5); print "$ar[5]:".($a-500)."-".($b+500)'  | awk '{ s=s" "$1} END { print s }' | perl ~/Bin/vargene.mergeOverlappingSamtoolsRegions.pl i)
+region1=$(egrep "stevor|PIR|PfEMP1" $LITTLEPF3D7 | \
+			perl -nle '@ar=split(/\t/); @bb=split(/\.\./,$ar[4]);($a) = $bb[0] =~ /\(*(\d+)/; ($b)= $bb[(scalar(@bb)-1)] =~ /(\d+)\)*/; $chr=substr($ar[0],3,5); print "Pf3$chr:".($a-500)."-".($b+500)'  | \
+			awk '{ s=s" "$1} END { print s }' | \
+			perl $MERGEOSAM )
 
-#region=$(egrep "stevor|PIR|PfEMP1" /nfs/pathogen003/tdo/Pfalciparum/3D7/Reference/Feb2015/little.Pf3D7.Feb2015.txt | perl -nle '@ar=split(/\t/); @bb=split(/\.\./,$ar[4]);($a) = $bb[0] =~ /\(*(\d+)/; ($b)= $bb[(scalar(@bb)-1)] =~ /(\d+)\)*/; $chr=substr($ar[0],3,5); print "Pf3$chr\_v3:".($a-500)."-".($b+500)'  | awk '{ s=s" "$1} END { print s }' | perl ~/Bin/vargene.mergeOverlappingSamtoolsRegions.pl )
+region=$(egrep "stevor|PIR|PfEMP1" $LITTLEPF3D7 | \
+			perl -nle '@ar=split(/\t/); @bb=split(/\.\./,$ar[4]);($a) = $bb[0] =~ /\(*(\d+)/; ($b)= $bb[(scalar(@bb)-1)] =~ /(\d+)\)*/; $chr=substr($ar[0],3,5); print "Pf3$chr\_v3:".($a-500)."-".($b+500)'  | \
+			awk '{ s=s" "$1} END { print s }' | \
+			perl $MERGEOSAM )
+
+region2=$(egrep "stevor|PIR|PfEMP1|rif" $LITTLEPF3D7V4 | \
+			perl -nle '@ar=split(/\t/); @bb=split(/\.\./,$ar[4]);($a) = $bb[0] =~ /\(*(\d+)/; ($b)= $bb[(scalar(@bb)-1)] =~ /(\d+)\)*/; $chr=substr($ar[0],3,5); print "$ar[5]:".($a-500)."-".($b+500)'  | \
+			awk '{ s=s" "$1} END { print s }' | \
+			perl $MERGEOSAM i)
+
+#region=$(egrep "stevor|PIR|PfEMP1" $LITTLEPF3D7 | \
+			# perl -nle '@ar=split(/\t/); @bb=split(/\.\./,$ar[4]);($a) = $bb[0] =~ /\(*(\d+)/; ($b)= $bb[(scalar(@bb)-1)] =~ /(\d+)\)*/; $chr=substr($ar[0],3,5); print "Pf3$chr\_v3:".($a-500)."-".($b+500)'  | \
+			# awk '{ s=s" "$1} END { print s }' | \
+			# perl $MERGEOSAM )
 
 #echo $region
 
 if [ "0" == "1" ] ; then
 	#rm NonMapped.$bam.sam MappedVAR.$bam.sam
-	for x in `ls /nfs/team112_data00/pf.bwa/bam/$name*bam` ; do 
+	for x in `ls $BAMFOLDER/$name*bam`; do 
 		echo " getting reads from $x";
-		samtools view -F 4 $x $region $region1 $region2 >> MappedVAR.$bam.sam
-		samtools view -f 4 $x >> NonMapped.$bam.sam
-insertSize=$(samtools view -f 2 $x  MAL14:200000-1000000 | cut -f 9 | sort -n | awk 'BEGIN{c=0;} {if($1>0) {a[c++]=$1; }} END{ median=a[int(c/2)]; print median}')
-#insertSize=$(samtools view -f 2 /nfs/team112_data00/pf.bwa/bam/$x  MAL14:200000-1000000 | cut -f 9 | sort -n | awk 'BEGIN{c=0;} {if($1>0) {a[c++]=$1; }} END{ median=a[int(c/2)]; print median}')
+		$SAMTOOLS view -F 4 $x $region $region1 $region2 >> MappedVAR.$bam.sam
+		$SAMTOOLS view -f 4 $x >> NonMapped.$bam.sam
+
+		insertSize=$($SAMTOOLS view -f 2 $x  MAL14:200000-1000000 | \
+						cut -f 9 | \
+						sort -n | \
+						awk 'BEGIN{c=0;} {if($1>0) {a[c++]=$1; }} END{ median=a[int(c/2)]; print median}')
+
+		#insertSize=$($SAMTOOLS view -f 2 $BAMFOLDER/$x  MAL14:200000-1000000 | \
+						# cut -f 9 | \
+						# sort -n | \
+						# awk 'BEGIN{c=0;} {if($1>0) {a[c++]=$1; }} END{ median=a[int(c/2)]; print median}')
 	done
-	RL=$(samtools view  $x  MAL14 | head -n 1 | cut -f 10 | perl -nle 'print length($_)');
+
+	RL=$($SAMTOOLS view  $x  MAL14 | head -n 1 | cut -f 10 | perl -nle 'print length($_)');
+
 else
 	# touch NonMapped.$bam.sam
 	if [ "1" == "1" ] ; then
 		echo "using local bam file "
-		samtools view  ../$bam $region $region1 $region2 > MappedVAR.$bam.sam
-### get all the relevant positions
+		$SAMTOOLS view  ../$bam $region $region1 $region2 > MappedVAR.$bam.sam
+		### get all the relevant positions
 
-### changed June 2019
-# insertSize set to 500 
-# RL adapter
-	samtools view -f 4 ../$bam > NonMapped.$bam.sam
-	insertSize=$(samtools view -f 2 ../$bam  Pf3D7_04_v3:200000-1000000 | cut -f 9 | sort -n | awk 'BEGIN{c=0;} {if($1>0) {a[c++]=$1; }} END{ median=a[int(c/2)]; print median}')
-RL=$(samtools view  ../$bam  | head -n 1 | cut -f 10 | perl -nle 'print length($_)');
+		### changed June 2019
+		# insertSize set to 500 
+		# RL adapter
+		$SAMTOOLS view -f 4 ../$bam > NonMapped.$bam.sam
+		insertSize=$($SAMTOOLS view -f 2 ../$bam  Pf3D7_04_v3:200000-1000000 | \
+						cut -f 9 | \
+						sort -n | \
+						awk 'BEGIN{c=0;} {if($1>0) {a[c++]=$1; }} END{ median=a[int(c/2)]; print median}')
+		
+		RL=$($SAMTOOLS view  ../$bam  | \
+				head -n 1 | \
+				cut -f 10 | \
+				perl -nle 'print length($_)');
 	
-
 	else 
-#longer smal test  MAL4:169451-182412 MAL4:550144-624339  MAL4:936588-994282 MAL4:1156045-1185848
-		samtools view -F 4 ../$bam  MAL4:31153-71936  > MappedVAR.$bam.sam
-		insertSize=$(samtools view -f 2 ../$bam  MAL14:200000-1000000 | cut -f 9 | sort -n | awk 'BEGIN{c=0;} {if($1>0) {a[c++]=$1; }} END{ median=a[int(c/2)]; print median}')
-		RL=$(samtools view  ../$bam  MAL14 | head -n 1 | cut -f 10 | perl -nle 'print length($_)');
+		#longer smal test  MAL4:169451-182412 MAL4:550144-624339  MAL4:936588-994282 MAL4:1156045-1185848
+		$SAMTOOLS view -F 4 ../$bam  MAL4:31153-71936  > MappedVAR.$bam.sam
+		insertSize=$($SAMTOOLS view -f 2 ../$bam  MAL14:200000-1000000 | \
+						cut -f 9 | \
+						sort -n | \
+						awk 'BEGIN{c=0;} {if($1>0) {a[c++]=$1; }} END{ median=a[int(c/2)]; print median}')
+		
+		RL=$($SAMTOOLS view  ../$bam  MAL14 | \
+		head -n 1 | \
+		cut -f 10 | \
+		perl -nle 'print length($_)');
 	fi
 fi
 
 if [ -z $RL ] ; then 
-RL=100
+	RL=100
 fi 
+
 if [ -z $insertSize ] ; then 
-insertSize=500
+	insertSize=500
 fi 
 
 if [ "$RL" -gt 80 ] ; then
-kmer=71
+	kmer=71
 else
-kmer=61
+	kmer=61
 fi
 
-echo "insert size is $insertSize. The readlength is $RL. ";
+echo "insert size is $insertSize. The readlength is $RL. "
 echo "kmer is $kmer"
-cat MappedVAR.$bam.sam NonMapped.$bam.sam  | perl ~/Bin/sam2fastq.2files.pl rawread_tmp
-fastaq enumerate_names rawread_tmp_1.fastq rawreads_1.fastq
-fastaq enumerate_names rawread_tmp_2.fastq rawreads_2.fastq
+
+cat MappedVAR.$bam.sam NonMapped.$bam.sam  | perl $SAMTOFASTQ rawread_tmp
+
+$FASTAQ enumerate_names rawread_tmp_1.fastq rawreads_1.fastq
+$FASTAQ enumerate_names rawread_tmp_2.fastq rawreads_2.fastq
+
 mv rawread_tmpSE.fastq rawreadsSE.fastq
 rm rawread_tmp*fastq
 
-#cat MappedVAR.$bam.sam   | perl ~/Bin/sam2fastq.2files.pl $bam
+#cat MappedVAR.$bam.sam   | perl $SAMTOFASTQ $bam
 #rm MappedVAR.$bam.sam NonMapped.$bam.sam
 fastq=$bam
 
 ### read correction
-#sga preprocess  -m 51 --permute-ambiguous -f 3 -q 3 -p 1  rawreads_1.fastq rawreads_2.fastq  > tmp.fastq
+#$SGA preprocess -m 51 --permute-ambiguous -f 3 -q 3 -p 1 rawreads_1.fastq rawreads_2.fastq > tmp.fastq
 
-#sga index -t 2 --disk=2000000  tmp.fastq
-#sga correct -k 51 -x 3 -t 2  -o read.corrected.fastq tmp.fastq; 
-#perl ~tdo/Bin/join.solexaBack.actual.sga.pl read.corrected.fastq $fastq
+#$SGA index -t 2 --disk=2000000  tmp.fastq
+#$SGA correct -k 51 -x 3 -t 2  -o read.corrected.fastq tmp.fastq; 
+#perl $JOINSOLEX read.corrected.fastq $fastq
 
 ### kick off the masurca
-cp /lustre/scratch118/infgen/team133/tdo/Pfalciparum/VAR/Assembly.Version3/config.txt .
-/nfs/pathogen003/tdo/bin/MaSuRCA-2.3.2/bin/masurca config.txt
+cp $VARCONFIG config.txt
+$MASURCA config.txt
 ./assemble.sh
 
-cp CA/10-gapclose/genome.scf.fasta SC.$name.fasta
+cp $CAFOLDER/$GAPCLOSEFOLDER/$SCFFASTA SC.$name.fasta
 
-#mv CA/10-gapclose/ .
+#mv $GAPCLOSEFOLDER .
 # uncomment the next 1,2,4 lines
-rm -rf CA/
-rm  assemble.sh  config.txt  environment.sh gapClose.err genome.uid global_arrival_rate.txt guillaumeKUnitigsAtLeast32bases_all.fasta guillaumeKUnitigsAtLeast32bases_all.jump.fasta k_u_hash_0 *.sam meanAndStdevByPrefix.pe.txt  pA.linking.frg pA.renamed.fastq pe.cor.fa pe.cor.log pe_data.tmp pe.linking.fa quorum.err quorum_mer_db.jf rawreadsSE.fastq runCA1.out runCA2.out runCA3.out   super1.err superReadSequences_shr.frg tigStore.err unitig_cov.txt unitig_layout.txt 
+rm -rf $CAFOLDER
+rm assemble.sh config.txt environment.sh gapClose.err \
+	genome.uid global_arrival_rate.txt guillaumeKUnitigsAtLeast32bases_all.fasta \
+	guillaumeKUnitigsAtLeast32bases_all.jump.fasta k_u_hash_0 *.sam meanAndStdevByPrefix.pe.txt \
+	pA.linking.frg pA.renamed.fastq pe.cor.fa pe.cor.log pe_data.tmp pe.linking.fa quorum.err \
+	quorum_mer_db.jf rawreadsSE.fastq runCA1.out runCA2.out runCA3.out super1.err superReadSequences_shr.frg \
+	tigStore.err unitig_cov.txt unitig_layout.txt
+
 rm -rf work1
-#cp 10-gapclose/genome.scf.fasta SC.$name.fasta
-#rm -rf 10-gagclose
-#~/Bin/vargene.AnnotationVARgenes.assembly.V0.1.sh SC.$name.fasta empty.bam $name
+#cp $GAPCLOSEFOLDER/$SCFFASTA SC.$name.fasta
+#rm -rf $GAPCLOSEFOLDER
+#$ANNOTVARGENES SC.$name.fasta empty.bam $name
 
 cd ..
-bsub -R "select[type==X86_64 && mem > 9000] rusage[mem=9000,tmp=4000]" -M9000 -o Output/out.Improvement.$name.o -e Output/out.Improvement.$name.e -n 4 -R "span[hosts=1]" bash ~/Bin/vargene.AssemblyPipeline.masurca.PostImprovement.NoReadextraction.sh  $name
+$BSUB -R "select[type==X86_64 && mem > 9000] rusage[mem=9000,tmp=4000]" -M9000 \
+	-o Output/out.Improvement.$name.o -e Output/out.Improvement.$name.e -n 4 \
+	-R "span[hosts=1]" \
+	bash $MASPOSTIMP  $name
 
 ###now improve the assembly by default
 ### do de novo assembly using predifined k-mer of 71
 #for i in  61 71 81  ; do 
-#	NAME=k.denovo.$fastq.$i; 
-#	~/bin/velveth $NAME $i -shortPaired  -fastq -separate  "$fastq"_1.fastq "$fastq"_2.fastq ; 
-#	~tdo/bin/velveth.tdo $NAME $i -shortPaired  -fastq -separate  rawreads_1.fastq rawreads_2.fastq ; 
-#	echo "~/bin/velvetg $NAME -exp_cov auto -cov_cutoff 5 -very_clean yes -ins_length $insertSize  -min_contig_lgth 500 ; "
-#	~tdo/bin/velvetg.tdo $NAME -exp_cov auto -cov_cutoff 5 -very_clean yes -ins_length $insertSize  -min_contig_lgth 300 ;  
+#	NAME=k.denovo.$fastq.$i
+#	$VELVETH $NAME $i -shortPaired  -fastq -separate  "$fastq"_1.fastq "$fastq"_2.fastq 
+#	$VELVETHTDO $NAME $i -shortPaired  -fastq -separate  rawreads_1.fastq rawreads_2.fastq 
+#	echo "$VELVETG $NAME -exp_cov auto -cov_cutoff 5 -very_clean yes -ins_length $insertSize  -min_contig_lgth 500 "
+#	$VELVETGTDO $NAME -exp_cov auto -cov_cutoff 5 -very_clean yes -ins_length $insertSize  -min_contig_lgth 300  
 #done
 
 #if [ ! -f "k.denovo.$fastq.$kmer/contigs.fa" ] ; then
 #	echo "Error in velvet, maybe read not long enough"
 #	exit 125;
 #fi
-#~tdo/Bin/assstats.sh k.denovo.$fastq.$kmer/contigs.fa  > stats.VelvetAssembly.txt
+#$ASSSTATS k.denovo.$fastq.$kmer/contigs.fa  > stats.VelvetAssembly.txt
 
-
-
+################################################################
